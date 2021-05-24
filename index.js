e9f28d5b70efa06c7db26019bd4e040a8e03cf42
Ajurgs <ajjurgs5954@gmail.com>
2021-05-23 20:21:55 -0600

    added find employee function

--- index.js
+++ index.js
@@ -5,24 +5,52 @@ const inquirer = require("inquirer");
 
 // ask user for staring input
 function baseQuestions() {
-  inquirer.prompt({
-    type: "list",
-    message: "What would you like to do?",
-    name: "choice",
-    choices: [
-      "View All Employees",
-      "View Employees by Department",
-      "View Employees by Manager",
-      "View Department Expenses",
-      "Add Employee",
-      "Remove Employee",
-      "Update Employee",
-      "Add Role",
-      "Remove Role",
-      "Add Department",
-      "Remove Department",
-    ],
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you like to do?",
+      name: "choice",
+      choices: [
+        "View All Employees",
+        "View Employees by Department",
+        "View Employees by Manager",
+        "View Department Expenses",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee",
+        "Add Role",
+        "Remove Role",
+        "Add Department",
+        "Remove Department",
+      ],
+    })
+    .then((res) => {
+      switch (res.choice) {
+        case "View All Employees":
+          viewEmployee();
+          break;
+        case "View Employees by Department":
+          break;
+        case "View Employees by Manager":
+          break;
+        case "View Department Expenses":
+          break;
+        case "Add Employee":
+          break;
+        case "Remove Employee":
+          break;
+        case "Update Employee":
+          break;
+        case "Add Role":
+          break;
+        case "Remove Role":
+          break;
+        case "Add Department":
+          break;
+        case "Remove Department":
+          break;
+      }
+    });
 }
 
 // add-------------------
@@ -33,6 +61,12 @@ function baseQuestions() {
 // depatrments
 // roles
 // employees
+function viewEmployee() {
+  connection.query("SELECT * FROM employee", (err, res) => {
+    if (err) throw err;
+    console.table(res);
+  });
+}
 // employees by manager (optional)
 // budget of a department
 // Update-------------
