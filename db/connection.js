e40249b4e96c20b0d9f4cd69e05311f43e72184e
Ajurgs <ajjurgs5954@gmail.com>
2021-05-24 14:35:23 -0600

    added more recursive functions

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "employees",
+  database: process.env.DB_NAME,
 });
 
 connection.connect();
