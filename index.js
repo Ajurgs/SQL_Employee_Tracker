7998adb63de9a8762d731e21c37c7ab629b04c59
Ajurgs <ajjurgs5954@gmail.com>
2021-05-23 22:11:13 -0600

    created the view employee by manager function

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("console.table");
 const connection = require("./db/connection");
 const inquirer = require("inquirer");
 
+baseQuestions();
+
 // ask user for staring input
 function baseQuestions() {
   inquirer
@@ -32,6 +34,7 @@ function baseQuestions() {
         case "View Employees by Department":
           break;
         case "View Employees by Manager":
+          viewEmployeeByManager();
           break;
         case "View Department Expenses":
           break;
@@ -62,12 +65,45 @@ function baseQuestions() {
 // roles
 // employees
 function viewEmployee() {
-  connection.query("SELECT * FROM employee", (err, res) => {
-    if (err) throw err;
-    console.table(res);
-  });
+  connection.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+    (err, res) => {
+      if (err) throw err;
+      console.table(res);
+    }
+  );
 }
 // employees by manager (optional)
+function viewEmployeeByManager() {
+  let managers = [];
+  connection.query(
+    "SELECT CONCAT (first_name, ' ', last_name) as manager FROM employee WHERE is_manager = true;",
+    (err, res) => {
+      if (err) throw err;
+      res.forEach((manager) => {
+        managers.push(manager.manager);
+      });
+      inquirer
+        .prompt({
+          type: "list",
+          message: "What Managers Employees would you like to view?",
+          name: "name",
+          choices: managers,
+        })
+        .then((res) => {
+          connection.query(
+            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id WHERE CONCAT(manager.first_name, ' ', manager.last_name) = ?",
+            res.name,
+            (err, res) => {
+              if (err) throw err;
+              console.table(res);
+            }
+          );
+        });
+    }
+  );
+}
+
 // budget of a department
 // Update-------------
 // employee roles
