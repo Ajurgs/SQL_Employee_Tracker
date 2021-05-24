edfca5e0ee9842e4c38aa4ac45e344b3bf35f6cb
Ajurgs <ajjurgs5954@gmail.com>
2021-05-23 20:09:22 -0600

    started with the js inquirer

--- index.js
+++ index.js
@@ -2,3 +2,43 @@
 require("console.table");
 const connection = require("./db/connection");
 const inquirer = require("inquirer");
+
+// ask user for staring input
+function baseQuestions() {
+  inquirer.prompt({
+    type: "list",
+    message: "What would you like to do?",
+    name: "choice",
+    choices: [
+      "View All Employees",
+      "View Employees by Department",
+      "View Employees by Manager",
+      "View Department Expenses",
+      "Add Employee",
+      "Remove Employee",
+      "Update Employee",
+      "Add Role",
+      "Remove Role",
+      "Add Department",
+      "Remove Department",
+    ],
+  });
+}
+
+// add-------------------
+// departments
+// roles
+// employees
+// view-------------------
+// depatrments
+// roles
+// employees
+// employees by manager (optional)
+// budget of a department
+// Update-------------
+// employee roles
+// employee managers (optional)
+// Delete
+// departments
+// roles
+// employees
